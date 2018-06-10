05f953f79bd5761542db88652c335cd4de2fc446
vigneshwar-d <cloudofvikki@icloud.com>
2018-06-10 12:16:41 +0530

    Initial Commit

---
+++ CrytoCalcy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CrytoCalcy
+//
+//  Created by Vigneshwar Devendran on 10/06/18.
+//  Copyright © 2018 Vigneshwar Devendran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
