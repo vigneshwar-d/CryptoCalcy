260c337ae952d21bcb4069f0016b4138470be474
vigneshwar-d <cloudofvikki@icloud.com>
2018-06-12 20:00:03 +0530

    All Coding Finished

--- CrytoCalcy/CoinListViewController.swift
+++ CrytoCalcy/CoinListViewController.swift
@@ -13,23 +13,36 @@ class CoinListViewController: UIViewController, UITableViewDelegate, UITableView
     
     @IBOutlet weak var tableView: UITableView!
     var array = [Coins]()
+   // var filteredData = [Coins]()
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    //var isSearching = false
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.keyboardDismissMode = .interactive
         tableView.register(UINib(nibName: "CoinCell", bundle: nil), forCellReuseIdentifier: "coinCell")
         loadAllItems()
         
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "coinCell", for: indexPath) as! CoinCell
-        cell.coinIcon.image = UIImage(named: array[indexPath.row].imageName!)
-        cell.coinName.text = array[indexPath.row].name
-        cell.accessoryType = array[indexPath.row].selected == true ? .checkmark : .none
-        return cell
+//        if isSearching{
+//            cell.coinIcon.image = UIImage(named: filteredData[indexPath.row].imageName!)
+//            cell.coinName.text = filteredData[indexPath.row].name
+//            cell.accessoryType = filteredData[indexPath.row].selected == true ? .checkmark : .none
+//            return cell
+        //        }else{}
+            cell.coinIcon.image = UIImage(named: array[indexPath.row].imageName!)
+            cell.coinName.text = array[indexPath.row].name
+            cell.accessoryType = array[indexPath.row].selected == true ? .checkmark : .none
+            return cell
+        
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        if isSearching{
+//            return filteredData.count
+//        }
         return array.count
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -55,5 +68,26 @@ class CoinListViewController: UIViewController, UITableViewDelegate, UITableView
     @IBAction func close(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
-    
+//    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+//        if searchBar.text == nil || searchBar.text == "" {
+//            isSearching = false
+//            view.endEditing(true)
+//            tableView.reloadData()
+//        }else{
+//            isSearching = true
+//            filteredData = array.filter({($0.name?.contains(searchBar.text!))!})
+//            tableView.reloadData()
+//        }
+//    }
 }
+
+
+
+
+
+
+
+
+
+
+
--- CrytoCalcy/ViewController.swift
+++ CrytoCalcy/ViewController.swift
@@ -18,16 +18,19 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var currencyText: UITextField!
     //var array = [Coins]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
     var nameArray = [String]()
     var iconArray = [String]()
     var priceArray = [Double]()
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     var calculatedPriceArray = [Double]()
+    var defaultCurrencyValue = [DefaultPrice]()
+    var statusMessage = "Connecting..."
     override func viewWillAppear(_ animated: Bool) {
-        //super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
-        currencyText.text = "0"
-        //tableView.reloadData()
+        //currencyText.text = "0"
         nameArray.removeAll()
         iconArray.removeAll()
         priceArray.removeAll()
@@ -36,17 +39,32 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.register(UINib(nibName: "WatchList", bundle: nil), forCellReuseIdentifier: "watchCell")
         if UserDefaults.standard.string(forKey: "wasLaunchedFromSource") != nil{
             print("Has been launched before")
-        }else{
+            //let getPrice = DefaultPrice(context: context)
+            let request: NSFetchRequest<DefaultPrice> = DefaultPrice.fetchRequest()
+            do{
+                try defaultCurrencyValue = context.fetch(request)
+                
+            }catch{
+                print("Error fetching currency value \(error)")
+            }
+            currencyText.text = defaultCurrencyValue.first?.value
+        }
+        else{
             print("App not launched")
             setSourceBase()
+            let request: NSFetchRequest<DefaultPrice> = DefaultPrice.fetchRequest()
+            do{
+                try defaultCurrencyValue = context.fetch(request)
+                
+            }catch{
+                print("Error fetching currency value \(error)")
+            }
+            currencyText.text = defaultCurrencyValue.first?.value
             let defaults = UserDefaults.standard
             defaults.set(true, forKey: "wasLaunchedFromSource")
         }
         loadSelectedSources()
-//        _ = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(ViewController.loadPrices), userInfo: nil, repeats: true)
-
         loadPrices()
-        //loadArrays()
     }
     //MARK: - Table View Functions
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -54,7 +72,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         cell.coinName.text = nameArray[indexPath.row]
         cell.coinIcon.image = UIImage(named: iconArray[indexPath.row])
         if calculatedPriceArray == []{
-            cell.priceLabel.text = "Loading..."
+            cell.priceLabel.text = statusMessage
         }else{
             cell.priceLabel.text = String(calculatedPriceArray[indexPath.row])
         }
@@ -75,7 +93,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     //MARK: - Source
     func setSourceBase(){
         print("Set Source Base Called")
-        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+        let value = DefaultPrice(context: context)
+        value.value = "1"
         
         let btc = Coins(context: context)
         btc.name = "Bitcoin [BTC]"
@@ -100,7 +120,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         let ltc = Coins(context: context)
         ltc.name = "Litecoin [LTC]"
         ltc.imageName = "LTC"
-        ltc.selected = true
+        ltc.selected = false
         
         let ada = Coins(context: context)
         ada.name = "Cardano [ADA]"
@@ -177,6 +197,216 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         omg.imageName = "OMG"
         omg.selected = false
         
+        let nxt = Coins(context: context)
+        nxt.name = "NXT [NXT]"
+        nxt.imageName = "NXT"
+        nxt.selected = false
+        
+        let drgn = Coins(context: context)
+        drgn.name = "DragonChain [DRGN]"
+        drgn.imageName = "DRGN"
+        drgn.selected = false
+        
+        let knc = Coins(context: context)
+        knc.name = "Kyber Network [KNC]"
+        knc.imageName = "KNC"
+        knc.selected = false
+        
+        let mona = Coins(context: context)
+        mona.name = "MonaCoin [MONA]"
+        mona.imageName = "MONA"
+        mona.selected = false
+        
+        let pivx = Coins(context: context)
+        pivx.name = "PIVX [PIVX]"
+        pivx.imageName = "PIVX"
+        pivx.selected = false
+        
+        let ardr = Coins(context: context)
+        ardr.name = "Ardor [ARDR]"
+        ardr.imageName = "ARDR"
+        ardr.selected = false
+        
+        let ark = Coins(context: context)
+        ark.name = "Ark [ARK]"
+        ark.imageName = "ARK"
+        ark.selected = false
+        
+        let kmd = Coins(context: context)
+        kmd.name = "Komodo [KMD]"
+        kmd.imageName = "KMD"
+        kmd.selected = false
+        
+        let bat = Coins(context: context)
+        bat.name = "BAT [BAT]"
+        bat.imageName = "BAT"
+        bat.selected = false
+        
+        let dgd = Coins(context: context)
+        dgd.name = "DigixDAO [DGD]"
+        dgd.imageName = "DGD"
+        dgd.selected = false
+        
+        let hsr = Coins(context: context)
+        hsr.name = "Hshare [HSR]"
+        hsr.imageName = "HSR"
+        hsr.selected = false
+        
+        let wtc = Coins(context: context)
+        wtc.name = "Walton Chain [WTC]"
+        wtc.imageName = "WTC"
+        wtc.selected = false
+        
+        let iost = Coins(context: context)
+        iost.name = "IOSToken [IOST]"
+        iost.imageName = "IOST"
+        iost.selected = false
+        
+        let snt = Coins(context: context)
+        snt.name = "Status [SNT]"
+        snt.imageName = "SNT"
+        snt.selected = false
+        
+        let dgb = Coins(context: context)
+        dgb.name = "DigiByte [DGB]"
+        dgb.imageName = "DGB"
+        dgd.selected = false
+        
+        let gnt = Coins(context: context)
+        gnt.name = "Golem [GNT]"
+        gnt.imageName = "GNT"
+        gnt.selected = false
+        
+        let btcp = Coins(context: context)
+        btcp.name = "Bitcoin Private [BTCP]"
+        btcp.imageName = "BTCP"
+        btcp.selected = false
+        
+        let doge = Coins(context: context)
+        doge.name = "Dogecoin [DOGE]"
+        doge.imageName = "DOGE"
+        doge.selected = false
+        
+        let rhoc = Coins(context: context)
+        rhoc.name = "RChain [RHOC]"
+        rhoc.imageName = "RHOC"
+        rhoc.selected = false
+        
+        let rep = Coins(context: context)
+        rep.name = "Augur [REP]"
+        rep.imageName = "REP"
+        rep.selected = false
+        
+        let bcd = Coins(context: context)
+        bcd.name = "Bitcoin Diamond [BCD]"
+        bcd.imageName = "BCD"
+        bcd.selected = false
+        
+        let waves = Coins(context: context)
+        waves.name = "Waves [WAVES]"
+        waves.imageName = "WAVES"
+        waves.selected = false
+        
+        let wan = Coins(context: context)
+        wan.name = "Wanchain [WAN]"
+        wan.imageName = "WAN"
+        wan.selected = false
+        
+        let mkr = Coins(context: context)
+        mkr.name = "Maker [MKR]"
+        mkr.imageName = "MKR"
+        mkr.selected = false
+        
+        let ppt = Coins(context: context)
+        ppt.name = "Populous [PPT]"
+        ppt.imageName = "PPT"
+        ppt.selected = false
+        
+        let strat = Coins(context: context)
+        strat.name = "Stratis [STRAT]"
+        strat.imageName = "STRAT"
+        strat.selected = false
+        
+        let bts = Coins(context: context)
+        bts.name = "BitShares [BTS]"
+        bts.imageName = "BTS"
+        bts.selected = false
+        
+        let zrx = Coins(context: context)
+        zrx.name = "OX [ZRX]"
+        zrx.imageName = "ZRX"
+        zrx.selected = false
+        
+        let btm = Coins(context: context)
+        btm.name = "Bytom [BTM]"
+        btm.imageName = "BTM"
+        btm.selected = false
+        
+        let sc = Coins(context: context)
+        sc.name = "Siacoin [SC]"
+        sc.imageName = "SC"
+        sc.selected = false
+        
+        let xvg = Coins(context: context)
+        xvg.name = "Verge [XVG]"
+        xvg.imageName = "XVG"
+        xvg.selected = false
+        
+        let ae = Coins(context: context)
+        ae.name = "Aeternity [AE]"
+        ae.imageName = "AE"
+        ae.selected = false
+        
+        let dcr = Coins(context: context)
+        dcr.name = "Decred [DCR]"
+        dcr.imageName = "DCR"
+        dcr.selected = false
+        
+        let ont = Coins(context: context)
+        ont.name = "Ontology [ONT]"
+        ont.imageName = "ONT"
+        ont.selected = false
+        
+        let steem = Coins(context: context)
+        steem.name = "Steem [STEEM]"
+        steem.imageName = "STEEM"
+        steem.selected = false
+        
+        let btg = Coins(context: context)
+        btg.name = "Bitcoin Gold [BTG]"
+        btg.imageName = "BTG"
+        btg.selected = false
+        
+        let zil = Coins(context: context)
+        zil.name = "Zilliqa [ZIL]"
+        zil.imageName = "ZIL"
+        zil.selected = false
+        
+        let icx = Coins(context: context)
+        icx.name = "ICON [ICX]"
+        icx.imageName = "ICX"
+        icx.selected = false
+        
+        let zec = Coins(context: context)
+        zec.name = "Zcash [ZEC]"
+        zec.imageName = "ZEC"
+        zec.selected = true
+        
+        let qtum = Coins(context: context)
+        qtum.name = "QTUM [QTUM]"
+        qtum.imageName = "QTUM"
+        qtum.selected = false
+        
+        let bcn = Coins(context: context)
+        bcn.name = "Bytecoin [BCN]"
+        bcn.imageName = "BCN"
+        bcn.selected = false
+        
+        let bnb = Coins(context: context)
+        bnb.name = "Binance Coin [BNB]"
+        bnb.imageName = "BNB"
+        bnb.selected = false
+        
         do{
             try context.save()
         }catch{
@@ -238,12 +468,13 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                 }
                 if NetworkReachabilityManager()?.isReachable == false{
                     print("No Internet")
-                    let alert = UIAlertController(title: "Oops!", message: "No Internet Connection", preferredStyle: .alert)
-                    let action = UIAlertAction(title: "Settings", style: .default) { (action) in
-                        UIApplication.shared.open(URL(string:UIApplicationOpenSettingsURLString)!)
-                    }
-                    alert.addAction(action)
-                    self.present(alert, animated: true, completion: nil)
+//                    let alert = UIAlertController(title: "Oops!", message: "No Internet Connection", preferredStyle: .alert)
+//                    let action = UIAlertAction(title: "Settings", style: .default) { (action) in
+//                        UIApplication.shared.open(URL(string:UIApplicationOpenSettingsURLString)!)
+//                    }
+//                    alert.addAction(action)
+//                    self.present(alert, animated: true, completion: nil)
+                    statusMessage = "No Internet"
                 }
        
     }
@@ -255,6 +486,14 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         print("Something is happening")
         calculatedPriceArray.removeAll()
         var it = 0
+//        let saveDefault = DefaultPrice(context: context)
+//        saveDefault.value = ""
+//        saveDefault.value = currencyText.text
+        do{
+            try context.save()
+        }catch{
+            print("Error saving default value \(error)")
+        }
         for _ in 0..<nameArray.count{
             if currencyText.text != ""{
             let calculatedValue = Double(currencyText.text!)! * Double(priceArray[it])
@@ -274,6 +513,11 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     @IBAction func refreshButton(_ sender: Any) {
+        loadPrices()
+    }
+    
+    @IBAction func clearButton(_ sender: Any) {
+        currencyText.text = "0"
         loadPrices()
     }
     
